8c09cf485ecd738d43b6aadc8018ba55bc1e7737
Ryan Pethel <arpethel@gmail.com>
2019-06-04 11:27:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,19 +4,19 @@ class App < Sinatra::Base
   get '/name' do
   "My name is Ryan"
 
-  erb :'name/index.html.erb'
+  # erb :'name/index.html.erb'
   end
 
   get '/hometown' do
   "My hometown is Atlanta"
 
-  erb :'hometown/index.html.erb'
+  # erb :'hometown/index.html.erb'
   end
 
   get '/favorite-song' do
   "My favorite-song is Like A Stone"
 
-  erb :'favorite-song/index.html.erb'
+  # erb :'favorite-song/index.html.erb'
   end
 
 
